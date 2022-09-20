71005dd346206fd2d38c38db733d80c931e3bcd3
Lyrthras <defelixanne@gmail.com>
2022-09-20 13:00:50 +0800

    Update package.lua

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
-    name = "lyrth/nec-server-checker",
-    version = "1.0.0",
+    name = "drasouls/nec-server-checker",
+    version = "1.1.0",
     dependencies = {
         "luvit/secure-socket@1.0.0",
         "creationix/coro-http@3.2.3",
